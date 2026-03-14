b394e88792f5bfa669d0a577d5dcbc8d9bd357aa
Traincape <traincapetech0641@gmail.com>
2026-03-14 17:52:31 +0530

    Gift_Transaction, auth service

--- index.js
+++ index.js
@@ -6,14 +6,9 @@ import 'react-native-gesture-handler';
 import {enableScreens} from 'react-native-screens';
 
 enableScreens();
-
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import messaging from '@react-native-firebase/messaging';
-import {handleBackgroundMessage} from './src/services/notifications/backgroundHandler';
-
-// Register background handler
-messaging().setBackgroundMessageHandler(handleBackgroundMessage);
 
 AppRegistry.registerComponent(appName, () => App);
+
